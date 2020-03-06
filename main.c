2a920df026a2e3ad6980c9fb2c6d4dc428ba073a
yurilima23 <yurilima93@hotmail.com>
2020-03-06 11:24:44 -0300

    tentando arrumar tudo

--- main.c
+++ main.c
@@ -18,29 +18,49 @@ typedef struct
     char state[2];
 } Process;
 
- Process espera[3];
- Process pronto[3];
- Process exec;
+Process espera[3];
+Process pronto[3];
+Process exec;
+Process vetAux[3];
+
+
+void listProntos()
+{
+    printf("Lista de Processos Prontos : \n ");
+    if (pronto[0].pid == 0)
+    {
+        pronto[0] = pronto[1];
+        pronto[1] = pronto[2];
+        pronto[2] = vetAux[0];
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Posição %d  PID: %ld \n ", i, pronto[i].pid);
+    }
+}
 
 void stateMode(Process p)
 {
-    
-    if ( strcmp(p.state, "p") == 0 || strcmp(p.state, "P") == 0)
+
+    if (strcmp(p.state, "p") == 0 || strcmp(p.state, "P") == 0)
     {
-         printf("Processo pid : %ld Esta Pronto \n",p.pid );
+        printf("Processo pid : %ld Esta Pronto \n", p.pid);
+        listProntos();
     }
-    if (strcmp (p.state, "ex") == 0 ||strcmp (p.state, "Ex") == 0 )
+    if (strcmp(p.state, "ex") == 0 || strcmp(p.state, "Ex") == 0)
     {
+
+        printf("Processo pid : %ld Esta em Execução \n", p.pid);
         
-        printf("Processo pid : %ld Esta em Execução \n",p.pid );
     }
-   if (strcmp (p.state, "e") == 0 || strcmp (p.state, "e")== 0) 
+    if (strcmp(p.state, "e") == 0 || strcmp(p.state, "e") == 0)
     {
-         printf("Processo pid : %ld Esta em Espera \n",p.pid );
+        printf("Processo pid : %ld Esta em Espera \n", p.pid);
     }
-    if (strcmp (p.state, "n") == 0 || strcmp (p.state, "N")== 0)
+    if (strcmp(p.state, "n") == 0 || strcmp(p.state, "N") == 0)
     {
-        printf("Processo pid : %ld Esta sendo Criado \n",p.pid );
+        printf("Processo pid : %ld Esta sendo Criado \n", p.pid);
     }
 }
 
@@ -53,54 +73,57 @@ int sumProcessTime(Process p[3])
     }
     return soma;
 }
-void listProntos(){
-   printf("Lista de Processos Prontos : \n");
-   for (int i = 0; i < 3; i++){
-      printf("Posição %d  PID: %ld \n",i,pronto[i].pid);
-   }
-}
- int listExec(int i){
-        if ((exec.cpu1 - i) == 0){
-            printf("Processo PID : %ld esta FINALIZADO \n", exec.pid);
-           return  1;
-        }else{
-            printf("Processos na lista de execução : \n");
-            printf("INTEIRO : %d \n" , i);
-            printf("PID: %ld  Tempo de execução : %d \n", exec.pid , exec.cpu1 - i );
-            return 0;
-        }
-            
+int listExec(int i)
+{
 
-
+    if ((exec.cpu1 - 1) == 0 || (exec.cpu1 - 1) < 0)
+    {
+        printf("Processo PID : %ld esta FINALIZADO \n", exec.pid);
+        return 1;
+    }
+    else
+    {
+        printf("Processos na lista de execução : \n");
+        printf("PID: %ld  Tempo de execução : %d \n", exec.pid, exec.cpu1 - 1);
+        return 0;
+    }
 }
-void listEspera(){
-       printf("Lista de Processos em Espera : \n");
-   for (int i = 0; i < 3; i++){
-      printf("Posição %d  PID: %ld \n",i,espera[i].pid);
-   }
+void listEspera()
+{
+    printf("Lista de Processos em Espera : \n");
+    if (espera[0].pid == 0)
+    {
+        espera[0] = espera[1];
+        espera[1] = espera[2];
+        espera[2] = vetAux[0];
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Posição %d  PID: %ld \n", i, espera[i].pid);
+    }
 }
 
-void generatorTable(Process p[3]){
-  
+void generatorTable(Process p[3])
+{
+
     printf("+--------+-----+--------+--------+-----+-----+---+                                       |\n");
     printf("| .::Informações do process::.                                                           |\n");
     printf("+--------+-----+--------+--------+-----+-----+---+                                       |\n");
     printf("| Pid     |    Time In       |    CPU 1       |  IN/OUT         |    CPU2         |\n");
     printf("|-----    |                  |                |                 |                 ||\n");
-    printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n",p[0].pid,p[0].time,p[0].cpu1,p[0].inOut,p[0].cpu2);
-    printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n",p[1].pid,p[1].time,p[1].cpu1,p[1].inOut,p[1].cpu2);
-    printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n",p[2].pid,p[2].time,p[2].cpu1,p[2].inOut,p[2].cpu2);
-   
-    
+    printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n", p[0].pid, p[0].time, p[0].cpu1, p[0].inOut, p[0].cpu2);
+    printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n", p[1].pid, p[1].time, p[1].cpu1, p[1].inOut, p[1].cpu2);
+    printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n", p[2].pid, p[2].time, p[2].cpu1, p[2].inOut, p[2].cpu2);
 }
+
 int main()
 {
     Process vet[3];
-    
+
     int min = 100000;
-    int x = 0,k=0;
+    int x = 0, k = 0;
     bool t = false;
-    Process vetAux[3];
+
     Process p1, p2, p3, psAux;
 
     pronto[0] = p1;
@@ -120,7 +143,6 @@ int main()
     printf("Digite Tempo de Processamento 2 do Processo P1 :");
     scanf("%d", &p1.cpu2);
 
-    
     // // Process P2 =============================
     printf("Digite Tempo de Chegada do Processo P2 :");
     scanf("%d", &p2.time);
@@ -150,65 +172,53 @@ int main()
     generatorTable(vet);
 
     // // Find min Number
-    // for (int i = 0; i <= 2; i++)
-    // {
-    //     if (vet[i].time < min)
-    //     {
-    //         min = vet[i].pid;
-    //     }
-    // }
-   
+    for (int i = 0; i <= 2; i++)
+    {
+        if (vet[i].time < min)
+        {
+            min = vet[i].time;
+        }
+    }
 
     // start execute
     printf("Inciando Gerenciamento de Processos : \n");
     printf("Processo do Sistema em execução \n");
     printf(" ================================================================ \n");
-    
+    int w = 0;
     for (int i = 0; i < sumProcessTime(vet); i++)
     {
-        if (i == vet[i].time){
-            strcpy(vet[i].state,"p");
-            pronto[x] = vet[i];
+        if (i == vet[x].time && t == false)
+        {
+            strcpy(vet[x].state, "p");
+            pronto[x] = vet[x];
             stateMode(pronto[x]);
-
-            listProntos();
             printf("\n \n ");
+            (x == 2) ? x = 0 : x++;
+            t = true;
+        }
+        if (t == true)
+        {
+            printf("Começou a execução \n");
             exec = pronto[0];
             pronto[0] = vetAux[0];
-            
-            strcpy(exec.state,"ex");
+            strcpy(exec.state, "ex");
             stateMode(exec);
-            listProntos();
-            printf("\n \n ");
-            
-            (x == 2 ) ? x = 0 : x++;
-           t = true;
-            
-        }
-        if (t == false){
             int d = listExec(i);
-            if(d == 1){
+            if (d == 1)
+            {
                 break;
             }
-        }else{
-            
-            espera[k] = exec;
-            strcpy(espera[k].state,"e");
-            stateMode(espera[k]);
             listProntos();
-            listEspera();
-            exec = pronto[0];
-            strcpy(exec.state,"ex");
-            stateMode(exec);
-            k++;
             t = false;
-
-
         }
-
-
+        else
+        {
+            printf("Seguiu Processo \n");
+            int d = listExec(i);
+            if (d == 1)
+            {
+                continue;
+            }
+        }
     }
-   
-    
-    
 }
