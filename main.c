b0755234d2d0599265a10ac1f49e9908ce2d549e
yurilima23 <yurilima93@hotmail.com>
2020-03-09 11:54:43 -0300

    qualquer coisa

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+// http://www.inf.ufg.br/~hebert/disc/aed1/AED1_09_Pilas.pdf pode ajudar ;
+//  https://www.cos.ufrj.br/~rfarias/cos121/pilhas.html
+// https://www.cprogressivo.net/2014/05/Filas-em-C-Como-Programar-Tutorial-Estrutura-de-Dados-Dinamica-Queue.html
 // build table
 #define SEPARADOR -10
 
@@ -15,68 +18,84 @@ typedef struct
     int cpu1;
     int inOut;
     int cpu2;
-    char state[2];
 } Process;
 
-Process espera[3];
-Process pronto[3];
-Process exec;
-Process vetAux[3];
-
-
-void listProntos()
+typedef struct
 {
-    printf("Lista de Processos Prontos : \n ");
-    if (pronto[0].pid == 0)
+    int qtd;
+    Process p[3];
+} Pilha;
+
+Pilha pilha;
+
+Pilha *createPilha();
+void freePilha(Pilha *pi);
+Process firstPositionPilha();
+int insertPilha(Pilha *pi, Process p);
+int removePilha(Pilha *pi);
+void listPilha(Pilha *pi);
+int lengthPilha(Pilha *pi);
+
+int j = 0;
+
+Pilha *createPilha()
+{
+    Pilha *pi;
+    pi = (Pilha *)malloc(sizeof(Pilha));
+    if (pi != NULL)
     {
-        pronto[0] = pronto[1];
-        pronto[1] = pronto[2];
-        pronto[2] = vetAux[0];
+        pi->qtd = 0;
+    }
+    return pi;
+}
+Process firstPositionPilha(Pilha *pi){
+    return pi->p[0];
+
+}
+
+int insertPilha(Pilha *pi, Process p)
+{
+    if (pi == NULL)
+    {
+        return 0;
+    }
+    if (pi->qtd == 3)
+    {
+        return 0;
     }
 
-    for (int i = 0; i < 3; i++)
+    pi->p[pi->qtd] = p;
+    printf("PID : %ld Pronto \n \n", pi->p->pid);
+    pi->qtd++;
+    return 1;
+}
+int removePilha(Pilha *pi)
+{
+    if (pi == NULL || pi->qtd == 0)
     {
-        printf("Posição %d  PID: %ld \n ", i, pronto[i].pid);
+        return 0;
+    }
+    pi->qtd--;
+    return 1;
+}
+int lengthPilha(Pilha *pi)
+{
+    if (pi == NULL)
+    {
+        return -1;
+    }
+    else
+    {
+        return pi->qtd;
     }
 }
 
-void listEspera()
+void listPilha(Pilha *pi)
 {
-    printf("Lista de Processos em Espera : \n");
-    if (espera[0].pid == 0)
+    printf("Lista de Processos Pontos : \n");
+    for (int i = 0; i < pi->qtd; i++)
     {
-        espera[0] = espera[1];
-        espera[1] = espera[2];
-        espera[2] = vetAux[0];
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        printf("Posição %d  PID: %ld \n", i, espera[i].pid);
-    }
-}
-
-void stateMode(Process p)
-{
-
-    if (strcmp(p.state, "p") == 0 || strcmp(p.state, "P") == 0)
-    {
-        printf("Processo pid : %ld Esta Pronto \n", p.pid);
-        listProntos();
-    }
-    if (strcmp(p.state, "ex") == 0 || strcmp(p.state, "Ex") == 0)
-    {
-
-        printf("Processo pid : %ld Esta em Execução \n", p.pid);
-        
-    }
-    if (strcmp(p.state, "e") == 0 || strcmp(p.state, "e") == 0)
-    {
-        printf("Processo pid : %ld Esta em Espera \n", p.pid);
-        listEspera();
-    }
-    if (strcmp(p.state, "n") == 0 || strcmp(p.state, "N") == 0)
-    {
-        printf("Processo pid : %ld Esta sendo Criado \n", p.pid);
+        printf("Posição %d PID : %ld \n \n", i, pi->p[i].pid);
     }
 }
 
@@ -89,22 +108,6 @@ int sumProcessTime(Process p[3])
     }
     return soma;
 }
-int listExec(int i)
-{
-    exec.cpu1 = exec.cpu1 - 1;
-    if (exec.cpu1 == 0 || exec.cpu1 < 0)
-    {
-        printf("\n \n Processo PID : %ld esta FINALIZADO \n \n ", exec.pid);
-        return 1;
-    }
-    else
-    {
-        printf("\n Processos na lista de execução : \n");
-        printf("PID: %ld  Tempo de execução : %d \n \n", exec.pid, exec.cpu1);
-        return 0;
-    }
-}
-
 
 void generatorTable(Process p[3])
 {
@@ -121,21 +124,19 @@ void generatorTable(Process p[3])
 
 int main()
 {
-    Process vet[3];
+    Process vet[3], exec;
 
     int min = 100000;
-    int x = 0, k = 0;
     bool t = false;
-
+    Pilha *prontos = createPilha();
+    Pilha *espera = createPilha();
     Process p1, p2, p3, psAux;
 
-    pronto[0] = p1;
-    pronto[1] = p1;
-    pronto[2] = p1;
     p1.pid = rand() % 10000;
     p2.pid = rand() % 10000;
     p3.pid = rand() % 10000;
     psAux.pid = rand() % 10000;
+
     // // Process P1 ==============================
     printf("Digite Tempo de Chegada do Processo P1 :");
     scanf("%d", &p1.time);
@@ -174,81 +175,27 @@ int main()
     vet[2] = p3;
     generatorTable(vet);
 
-    // // Find min Number
-    for (int i = 0; i <= 2; i++)
-    {
-        if (vet[i].time < min)
-        {
-            min = vet[i].time;
-        }
-    }
-
     // start execute
     printf("Inciando Gerenciamento de Processos : \n");
     printf("Processo do Sistema em execução \n");
     printf(" ================================================================ \n");
-    int w = 0;
-    t = false;
+
+    int x = 0;
+
     for (int i = 0; i < sumProcessTime(vet); i++)
     {
-        if (i == vet[x].time && t == false)
+
+        if (vet[x].time == i)
         {
-            printf("Entrou um novo PID: \n");
-            if (pronto[0].pid == 0){
-                pronto[0] = vet[x];
-            }else if (pronto[1].pid == 0) {
-                 pronto[1] = vet[x];
-            }else{
-                 pronto[2] = vet[x];
-            }
-            strcpy(vet[x].state, "p");
-            stateMode(pronto[x]);
-            listProntos();
-            printf("\n \n ");
-            (x == 2) ? x = 0 : x++;
-            t = true;
+            insertPilha(prontos, vet[x]);
+            insertPilha(prontos, vet[1]);
+            listPilha(prontos);
         }
-        //tira dos prontos e inicia a execução
-        if (t == true)
-        {
-            printf("Começou a execução \n");
-            //esse if é para colocar na lista de espera
-            if (exec.pid != 0){
-                if ( espera[0].pid == 0){
-                espera[0] = exec;
-                }else if (espera[1].pid == 0) {
-                  espera[1] = exec;
-                }else{
-                 pronto[2] = exec;
-                }
-                espera[w] = exec;
-                exec = vetAux[0];
-                strcpy(espera[w].state,"e");
-                stateMode(espera[w]);
-                (w == 2) ? w = 0 : w++;
-            }
-            exec = pronto[0];
-            pronto[0] = vetAux[0];
-            strcpy(exec.state, "ex");
-            stateMode(exec);
-            int d = listExec(i);
-            if (d == 1)
-            {
-                break;
-            }
-            listProntos();
-            t = false;
-           
-        }
-        // segue a execução contando enquanto não entra um novo
-        else
-        {
-            printf("Seguiu Processo \n");
-            int j = listExec(i);
-            if (j == 1)
-            {
-                continue;
-            }
-        }
+    
+       exec = firstPositionPilha(prontos);
+       removePilha(prontos);
+       listPilha(prontos);
+       printf("O PID : %ld esta em execução \n \n", exec.pid);
+
     }
 }
