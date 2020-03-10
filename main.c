01d8db3ed557d9aefbab352ddd9b417fba8e7e81
yurilima23 <yurilima93@hotmail.com>
2020-03-10 11:57:16 -0300

    ordei no vetor para iniciar com o menor tempo

--- main.c
+++ main.c
@@ -28,6 +28,8 @@ typedef struct
 
 Pilha pilha;
 
+Process vet[3];
+
 Pilha *createPilha();
 void freePilha(Pilha *pi);
 Process firstPositionPilha();
@@ -48,9 +50,9 @@ Pilha *createPilha()
     }
     return pi;
 }
-Process firstPositionPilha(Pilha *pi){
+Process firstPositionPilha(Pilha *pi)
+{
     return pi->p[0];
-
 }
 
 int insertPilha(Pilha *pi, Process p)
@@ -109,93 +111,128 @@ int sumProcessTime(Process p[3])
     return soma;
 }
 
-void generatorTable(Process p[3])
+void orderPosition()
 {
-
-    printf("+--------+-----+--------+--------+-----+-----+---+                                       |\n");
-    printf("| .::Informações do process::.                                                           |\n");
-    printf("+--------+-----+--------+--------+-----+-----+---+                                       |\n");
-    printf("| Pid     |    Time In       |    CPU 1       |  IN/OUT         |    CPU2         |\n");
-    printf("|-----    |                  |                |                 |                 ||\n");
-    printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n", p[0].pid, p[0].time, p[0].cpu1, p[0].inOut, p[0].cpu2);
-    printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n", p[1].pid, p[1].time, p[1].cpu1, p[1].inOut, p[1].cpu2);
-    printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n", p[2].pid, p[2].time, p[2].cpu1, p[2].inOut, p[2].cpu2);
-}
-
-int main()
-{
-    Process vet[3], exec;
-
-    int min = 100000;
-    bool t = false;
-    Pilha *prontos = createPilha();
-    Pilha *espera = createPilha();
-    Process p1, p2, p3, psAux;
-
-    p1.pid = rand() % 10000;
-    p2.pid = rand() % 10000;
-    p3.pid = rand() % 10000;
-    psAux.pid = rand() % 10000;
-
-    // // Process P1 ==============================
-    printf("Digite Tempo de Chegada do Processo P1 :");
-    scanf("%d", &p1.time);
-    printf("Digite Tempo de Processamento do Processo P1 :");
-    scanf("%d", &p1.cpu1);
-    printf("Digite Tempo de Entrada/Saida do Processo P1 :");
-    scanf("%d", &p1.inOut);
-    printf("Digite Tempo de Processamento 2 do Processo P1 :");
-    scanf("%d", &p1.cpu2);
-
-    // // Process P2 =============================
-    printf("Digite Tempo de Chegada do Processo P2 :");
-    scanf("%d", &p2.time);
-    printf("Digite Tempo de Processamento do Processo P2 :");
-    scanf("%d", &p2.cpu1);
-    printf("Digite Tempo de Entrada/Saida do Processo P2 :");
-    scanf("%d", &p2.inOut);
-    printf("Digite Tempo de Processamento 2 do Processo P2 :");
-    scanf("%d", &p2.cpu2);
-
-    // // END Process P2 ==========================
-
-    // // Process P3 =============================
-    printf("Digite Tempo de Chegada do Processo P3 :");
-    scanf("%d", &p3.time);
-    printf("Digite Tempo de Processamento do Processo P3 :");
-    scanf("%d", &p3.cpu1);
-    printf("Digite Tempo de Entrada/Saida do Processo P3 :");
-    scanf("%d", &p3.inOut);
-    printf("Digite Tempo de Processamento 2 do Processo P3 :");
-    scanf("%d", &p3.cpu2);
-
-    // END Process P3 ==========================
-    vet[0] = p1;
-    vet[1] = p2;
-    vet[2] = p3;
-    generatorTable(vet);
-
-    // start execute
-    printf("Inciando Gerenciamento de Processos : \n");
-    printf("Processo do Sistema em execução \n");
-    printf(" ================================================================ \n");
-
-    int x = 0;
-
-    for (int i = 0; i < sumProcessTime(vet); i++)
+    int i, j;
+    Process aux;
+    for(int i = 0; i < 3; i++)
     {
-
-        if (vet[x].time == i)
+        for (int j = 0; j < 3; j++)
         {
-            insertPilha(prontos, vet[x]);
-            insertPilha(prontos, vet[1]);
-            listPilha(prontos);
+            if (vet[i].time < vet[j].time)
+            {
+                //aqui acontece a troca, do maior cara  vaia para a direita e o menor para a esquerda
+                aux = vet[i];
+                vet[i] = vet[j];
+                vet[j] = aux;
+            }
         }
-    
-       exec = firstPositionPilha(prontos);
-       removePilha(prontos);
-       listPilha(prontos);
-       printf("O PID : %ld esta em execução \n \n", exec.pid);
-
     }
 }
+    void generatorTable(Process p[3])
+    {
+
+        printf("+--------+-----+--------+--------+-----+-----+---+                                       |\n");
+        printf("| .::Informações do process::.                                                           |\n");
+        printf("+--------+-----+--------+--------+-----+-----+---+                                       |\n");
+        printf("| Pid     |    Time In       |    CPU 1       |  IN/OUT         |    CPU2         |\n");
+        printf("|-----    |                  |                |                 |                 ||\n");
+        printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n", p[0].pid, p[0].time, p[0].cpu1, p[0].inOut, p[0].cpu2);
+        printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n", p[1].pid, p[1].time, p[1].cpu1, p[1].inOut, p[1].cpu2);
+        printf("| %ld     |     %d           |    %d          |   %d            |   %d            ||\n", p[2].pid, p[2].time, p[2].cpu1, p[2].inOut, p[2].cpu2);
+    }
+
+    int main()
+    {
+        Process exec;
+
+        int min = 100000;
+        bool t = false;
+        Pilha *prontos = createPilha();
+        Pilha *espera = createPilha();
+        Process p1, p2, p3, psAux;
+
+        p1.pid = rand() % 10000;
+        p2.pid = rand() % 10000;
+        p3.pid = rand() % 10000;
+
+        // // Process P1 ==============================
+        printf("Digite Tempo de Chegada do Processo P1 :");
+        scanf("%d", &p1.time);
+        printf("Digite Tempo de Processamento do Processo P1 :");
+        scanf("%d", &p1.cpu1);
+        printf("Digite Tempo de Entrada/Saida do Processo P1 :");
+        scanf("%d", &p1.inOut);
+        printf("Digite Tempo de Processamento 2 do Processo P1 :");
+        scanf("%d", &p1.cpu2);
+
+        // // Process P2 =============================
+        printf("Digite Tempo de Chegada do Processo P2 :");
+        scanf("%d", &p2.time);
+        printf("Digite Tempo de Processamento do Processo P2 :");
+        scanf("%d", &p2.cpu1);
+        printf("Digite Tempo de Entrada/Saida do Processo P2 :");
+        scanf("%d", &p2.inOut);
+        printf("Digite Tempo de Processamento 2 do Processo P2 :");
+        scanf("%d", &p2.cpu2);
+
+        // // END Process P2 ==========================
+
+        // // Process P3 =============================
+        printf("Digite Tempo de Chegada do Processo P3 :");
+        scanf("%d", &p3.time);
+        printf("Digite Tempo de Processamento do Processo P3 :");
+        scanf("%d", &p3.cpu1);
+        printf("Digite Tempo de Entrada/Saida do Processo P3 :");
+        scanf("%d", &p3.inOut);
+        printf("Digite Tempo de Processamento 2 do Processo P3 :");
+        scanf("%d", &p3.cpu2);
+
+        // END Process P3 ==========================
+
+        vet[0] = p1;
+        vet[1] = p2;
+        vet[2] = p3;
+        orderPosition(vet);
+        generatorTable(vet);
+        exit;
+        printf("\n \n Esse é a ultima posição %ld \n \n ", vet[0].pid);
+        // start execute
+        printf("Inciando Gerenciamento de Processos : \n");
+        printf("Processo do Sistema em execução \n");
+        printf(" ================================================================ \n");
+
+        int x = 0;
+        bool j = true;
+
+        for (int i = 0; i < sumProcessTime(vet); i++)
+        {
+            printf(" esse é o I : %d \n", i);
+            if (vet[x].time == i)
+            {
+                printf("ENTROUUUUUUU  :");
+                insertPilha(prontos, vet[x]);
+                listPilha(prontos);
+                if (exec.pid == 0)
+                {
+                    exec = firstPositionPilha(prontos);
+                    removePilha(prontos);
+                }
+                j = false;
+                (x == 3) ? x = 0 : x++;
+            }
+            if (exec.cpu1 != 0)
+            {
+                printf("O PID : %ld esta em execução Tempo : %d \n \n", exec.pid, exec.cpu1--);
+            }
+            if (exec.cpu1 <= 0)
+            {
+                listPilha(prontos);
+                printf("O PID : %ld esta FINALIZADO \n \n", exec.pid);
+                j = true;
+                exec = psAux;
+                continue;
+            }
+            break;
+        }
+    }
